0074ddb41c5ad96dd0f431116f0bbcd3683ba0dd
bloxfruit25 <nood2proinbloxfruit@gmail.com>
2026-01-15 13:29:08 +0200

    Create resource-pack.js

---
+++ pages/resource-pack.js
@@ -0,0 +1,9 @@
+export default function RP(){
+  return (
+    <main>
+      <h1>Resource Pack</h1>
+      <p>The resource pack is private. Admin users only can download it via the admin API. The pack is NOT public on the site.</p>
+      <p>Installation instructions for tLauncher etc. are in the README.</p>
+    </main>
+  )
+}
